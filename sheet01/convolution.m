9d61081231a78ec58edcb3fbd6cefa191b8e9e96
Pierre Bréchet <brechetp@eurecom.fr>
2015-10-26 17:03:17 +0100

    corner replication for copy padding

--- sheet01/convolution.m
+++ sheet01/convolution.m
@@ -11,7 +11,7 @@ end
 for x=1:X
   for y=1:Y
     for z =1:3
-    J(x, y, z) = uint8(sum(sum(mask_s .* double(I_bordered(x:x+M-1, y:y+N-1, z)))));
+      J(x, y, z) = uint8(sum(sum(mask_s .* double(I_bordered(x:x+M-1, y:y+N-1, z)))));
+    end
   end
-end
 end
