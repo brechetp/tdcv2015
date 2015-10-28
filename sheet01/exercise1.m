23feeb84fce729e915dc41ea8af6a9ae91636c5b
Lukas von Stumberg <lukas@18.219.eduroam.dynamic.rbg.tum.de>
2015-10-28 12:30:05 +0100

    Mirrored padding should work now.

--- sheet01/exercise1.m
+++ sheet01/exercise1.m
@@ -5,7 +5,10 @@ I_mirror = padding(I, 25, 25, 0);
 I2_copy = padding(I2, 25, 25, 0);
 %mask = ones(25, 25);
 
-%J = convolution(I, [-1, -1, -1; -1, 8,  -1; -1, -1, -1], 1);
+J = convolution(I2, [-1, -1, -1; -1, 8,  -1; -1, -1, -1], 0)
 imshow(J)
 
+D = convolution(I, 0.9 .* ones(3, 3), 1);
+%imshow(D);
 
+ 
--- sheet01/padding.m
+++ sheet01/padding.m
@@ -19,7 +19,27 @@ case 0 % copy-the-border treatment
 
 case 1 % mirror the border treatment
     %I_bordered = padarray(I, [m, n], 'symmetric');
+    
+  I_bordered(1+m:m+X, 1+n:n+Y, :) = I(1:X, 1:Y, :);
+  border_xu = I(1:m, :, :);
+  border_xd = I(X-m+1:X, :, :);
+  border_yl = I(:, 1:n, :);
+  border_yr = I(:, Y-n+1:Y, :);
+  I_bordered(1:m,               (n+1):(n+Y), :) = flipud(border_xu);
+  I_bordered((m+X+1):(M+X-1),   (n+1):(n+Y), :) = flipud(border_xd);
+  I_bordered((m+1):(m+X),       1:n, :) = fliplr(border_yl);
+  I_bordered((m+1):(m+X),       (n+Y+1):(N-1+Y), :) = fliplr(border_yr);
   
+  border_tl = I(1:m,    1:n, :);
+  border_tr = I(1:m,    Y-n+1:Y, :);
+  border_dl = I(X-m+1:X,1:n, :);
+  border_dr = I(X-m+1:X,Y-n+1:Y, :);
+  %disp(size(border_tr));
+  %disp(size(I_bordered(1:m, (n+1):(n+Y), :)));
+  I_bordered(1:m,               1:n, :) = flipud(fliplr(border_tl));
+  I_bordered(1:m,               (n+Y+1):(N-1+Y), :) = flipud(fliplr(border_tr));
+  I_bordered((m+X+1):(M+X-1),   1:n, :) = flipud(fliplr(border_dl));
+  I_bordered((m+X+1):(M+X-1),   (n+Y+1):(N-1+Y), :) = flipud(fliplr(border_dr));
 otherwise
     error('The padding style is not recognized');
 
