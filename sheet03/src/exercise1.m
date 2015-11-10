cdb22f79444eb79fdf0862a3731748ba78b8e0ce
Pierre Bréchet <brechetp@eurecom.fr>
2015-11-10 19:10:51 +0100

    display corner (pierre) add and sheet03 finished

--- sheet03/src/exercise1.m
+++ sheet03/src/exercise1.m
@@ -1,2 +1,4 @@
-house = im2double(imread('../../images/house.tif', 'tif'));
+corners = harris_laplace(img, k, s_0, N, alpha, threshold);
+Ic = display_corners(img, corners);
+imshow(Ic);
 
--- sheet03/src/harris.m
+++ sheet03/src/harris.m
@@ -4,13 +4,14 @@ Dx = repmat([-1:1], [3, 1]);
 Dy = Dx';
 sigma_I = k^n * s_0;
 sigma_D = 0.7 * sigma_I;
-gauss_D = fspecial('gaussian', max(1, fix(6*sigma_D)), sigma_D);
-gauss_I = fspecial('gaussian', max(1, fix(6*sigma_I)), sigma_I);
+gauss_D = fspecial('gaussian', round(6*sigma_D), sigma_D);
+gauss_I = fspecial('gaussian', round(6*sigma_I), sigma_I);
 Lx = conv2(conv2(I, Dx, 'same'), gauss_D, 'same'); % smoothed gradient according to x
 Ly = conv2(conv2(I, Dy, 'same'), gauss_D, 'same'); % smoothed gradient according to y
 
-M1 = conv2(Lx .^2, gauss_I, 'same'); % scaling convolution
-M2 = conv2(Ly.^2, gauss_I, 'same'); % to produce the M matrix
-M12 = conv2(Lx .* Ly, gauss_I, 'same');
+M1 = sigma_D^2*conv2(Lx .^2, gauss_I, 'same'); % scaling convolution
+M2 = sigma_D^2*conv2(Ly.^2, gauss_I, 'same'); % to produce the M matrix
+M12 = sigma_D^2*conv2(Lx .* Ly, gauss_I, 'same');
 R = (M1 .* M2 - M12 .^2) - alpha * (M1 + M2) .^2; % det(M) - alpha*tr(M)^2
+R(R<t) = 0;
 end
